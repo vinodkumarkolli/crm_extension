b48a0fd8f358acdc6908f89c4b97689c9332dc4d
vinodkumarkolli <vinodkumarkolli@gmail.com>
2025-03-09 08:49:02 +0000

    chore: modified zoom control on map

--- romeo/src/pages/Data.vue
+++ romeo/src/pages/Data.vue
@@ -21,20 +21,20 @@ function initMap() {
     mapResource.reload().then(response => {
         //pois = JSON.parse(JSON.stringify(response))
         const pois = JSON.parse(JSON.stringify(response))
-        console.log(pois)
+        if(mapContainer.value){
+            mapContainer.value = L.map('map', { zoomControl: false }).setView([13.009409, 80.151071], 12);
+            L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
+            minZoom: 0,
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            ext: 'png'
+            }).addTo(mapContainer.value);
+            L.control.zoom({ position: 'bottomright' }).addTo(mapContainer.value);
+        }
+        else {
+            console.error('Map container not found');
+        }
     });
-    if(mapContainer.value){
-        mapContainer.value = L.map('map').setView([13.009409, 80.151071], 12);
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'png'
-      }).addTo(mapContainer.value);
-    }
-    else {
-        console.error('Map container not found');
-    }
 }
 </script>
 <style scoped>
