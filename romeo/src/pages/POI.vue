fb70b593665b8ce968e1f1c4e21437d4ed11583f
vinodkumarkolli <vinodkumarkolli@gmail.com>
2025-03-26 08:58:30 +0000

    chore:disabled location field in POI.vue

--- romeo/src/pages/POI.vue
+++ romeo/src/pages/POI.vue
@@ -22,10 +22,10 @@
                     <p class="text-gray-600">WorkMate ID:</p>
                     <p>{{ workmate_id }}</p>
                 </div>
-                <div>
+                <!-- <div>
                     <p class="text-gray-600">Location:</p>
                     <p>{{ latitude }}, {{ longitude }}</p>
-                </div>
+                </div> -->
                 <div>
                     <p class="text-gray-600">POI Status:</p>
                     <p>{{ poi_status }}</p>
@@ -69,10 +69,10 @@
                         <p class="text-gray-600">Distance:</p>
                         <p id="toLocDistance" > {{ merge_to_location.radialDistance }} mts away</p>
                     </div>
-                    <div>
+                    <!-- <div>
                         <p class="text-gray-600">Location:</p>
                         <p id="toLocLocation" > {{ merge_to_location.latitude }}, {{ merge_to_location.longitude }}</p>
-                    </div>
+                    </div> -->
                     <div v-if="merge_to_location" class="col-span-2 flex justify-center ">
                         <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                             Submit
