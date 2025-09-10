2ba7e0334057aeb86b4c4c586d67f0d04da6b0ce
vinodkumarkolli <vinodkumarkolli@gmail.com>
2025-09-10 17:26:54 +0000

    chore: fix for comments on crm poi

--- crm_extension/crm_extension/doctype/crm_poi_notify/crm_poi_notify.js
+++ crm_extension/crm_extension/doctype/crm_poi_notify/crm_poi_notify.js
@@ -6,33 +6,33 @@ frappe.ui.form.on("CRM POI Notify", {
 		if (frm.doc.docstatus === 1) {
 			// Cancel logic implemented in Python file (on_cancel method)
 			// When Cancel is clicked, doc.crm_poi's poi_status is restored to "Active" and a comment is added
-			//add a custom button "Close POI", if doc.notification_type = 'Closure' and doc.is_validated = 0
-	       	if (frm.doc.notification_type === 'Closure' && frm.doc.is_validated === 0) {
-	           frm.add_custom_button(__('Close POI'), function() {
-				//before executing the below method call, prompt the user, if he is sure to close the poi. if yes clicked then execute the below method call else dont call
-				frappe.confirm(
-					__('Are you sure you want to close this POI? This action cannot be undone.'),
-					function() {
-						// User clicked "Yes"
-						frappe.call({
-							method: 'crm_extension.crm_extension.doctype.crm_poi_notify.crm_poi_notify.close_poi_by_name',
-							args: {
-								docname: frm.doc.name
-							},
-							callback: function(r) {
-								if (!r.exc) {
-									frm.reload_doc();
+			// Add a custom button "Request Closure" if doc.notification_type = 'Closure' and doc.docstatus === 1 (submitted)
+			if (frm.doc.notification_type === 'Closure' && frm.doc.docstatus === 1 && frm.doc.is_validated === 0) {
+				frm.add_custom_button(__('Close POI'), function() {
+					//before executing the below method call, prompt the user, if he is sure to close the poi. if yes clicked then execute the below method call else dont call
+					frappe.confirm(
+						__('Are you sure you want to close this POI? This action cannot be undone.'),
+						function() {
+							// User clicked "Yes"
+							frappe.call({
+								method: 'crm_extension.crm_extension.doctype.crm_poi_notify.crm_poi_notify.close_poi_by_name',
+								args: {
+									docname: frm.doc.name
+								},
+								callback: function(r) {
+									if (!r.exc) {
+										frm.reload_doc();
+									}
 								}
-							}
-						});
-					},
-					function() {
-						// User clicked "No" or closed the dialog
-						// Do nothing
-					}
-				);
-	           },__("Approvals"));
-	       	}
+							});
+						},
+						function() {
+							// User clicked "No" or closed the dialog
+							// Do nothing
+						}
+					);
+				}, __("Actions"));
+			}
 		}
 	},
 });
--- crm_extension/crm_extension/doctype/crm_poi_notify/crm_poi_notify.py
+++ crm_extension/crm_extension/doctype/crm_poi_notify/crm_poi_notify.py
@@ -7,16 +7,23 @@ from frappe.model.document import Document
 
 class CRMPOINotify(Document):
 	# implement after_submit hook where, self.notification_type == 'Closure', a comment is added to self.crm_poi of "Edit" type = Status changed to <b>Closure Requested</b>
-	def after_submit(self):
+	def on_submit(self):
+		# Debugging: Log that after_submit is being called
 		if self.notification_type == 'Closure':
-			# Add comment to CRM POI
-			frappe.get_doc({
-				'doctype': 'Comment',
-				'comment_type': 'Edit',
-				'reference_doctype': 'CRM POI',
-				'reference_name': self.crm_poi,
-				'content': 'Status changed to <b>Closure Requested</b>'
-			}).insert(ignore_permissions=True)
+			try:
+				# Add comment to CRM POI
+				comment_doc = frappe.get_doc({
+					'doctype': 'Comment',
+					'comment_type': 'Edit',
+					'reference_doctype': 'CRM POI',
+					'reference_name': self.crm_poi,
+					'content': 'Status changed to <b>Closure Requested</b>'
+				})
+				comment_doc.insert(ignore_permissions=True)
+			except Exception as e:
+				frappe.log_error(f"Error in after_submit for CRM POI Notify {self.name}: {str(e)}")
+				frappe.log_error(f"Error traceback: {frappe.get_traceback()}")
+				raise
 
 	def on_cancel(self):
 		if self.notification_type == 'Closure':
